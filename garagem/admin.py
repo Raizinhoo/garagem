a60cc6c734d87fb2e587f3a5aa8f2d8d312d8e73
Raizinhoo <gersinhowasd@gmail.com>
2023-03-14 16:37:04 -0300

    catapimascerto

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 
-from .models import Marca
+from .models import Categoria, Marca
 
-from .models import Categoria
-
-admin.site.register(Marca)
 admin.site.register(Categoria)
-
-
-# Register your models here.
+admin.site.register(Marca)
--- garagem/models.py
+++ garagem/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class Marca(models.Model):
-    descricao = models.CharField(max_length=100)
+    nome = models.CharField(max_length=50, )
+    nacionalidade = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
-        return self.descricao
+        return self.nome.upper()
 
 class Categoria(models.Model):
-    descricão = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.descricao
